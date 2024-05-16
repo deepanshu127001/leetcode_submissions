da291e14971ebc5101f60cdd2d8bd7fe92b3f1c2
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-17 01:45:21 +0530

    Create file

---
+++ 1-TwoSum/1-TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(value)!=mp.end())
+            {
+            int value= target-nums[i];
+        return {};
+                mp.insert({nums[i],i});
+                auto val=mp.find(value);
+            }
+    }
+                return {val->second,i};
+};
+[
