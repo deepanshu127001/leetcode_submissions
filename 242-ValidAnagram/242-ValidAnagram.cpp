dbd6c0ea4b8268dbaf58e0eae14b084044eea518
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-17 01:15:16 +0530

    Create file

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,18 @@
+        for(char ch:s)mp1[ch]++;
+        for(char ch:t)mp2[ch]++;
+
+        for(int i=0;i<s.length();i++){
+            //checking if character exists in both the maps
+        }
+            char ch= s[i];
+            if(mp2.find(ch)==mp2.end())return false;
+            else{
+                //extract count;
+            }
+                if(val1->second != val2->second )return false;
+        return true;
+    }
+                auto val1 = mp1.find(ch);
+                auto val2 = mp2.find(ch);
+};
+"
