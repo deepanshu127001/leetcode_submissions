fabe47833258778c1ceddfbabe5486cf8b9bdb96
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-17 00:12:50 +0530

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    nums.sort();
+    if(nums.length==1){
+        return false
+    }
+    for(let i=1;i<nums.length;i++){
+        if(nums[i-1]==nums[i])
+            return true
+    }
+    return false;
+};
+[1,2,3,1]
