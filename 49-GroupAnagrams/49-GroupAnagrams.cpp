38f1db0178acf24e54f001048db47bd98bd3fa7b
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-17 02:03:03 +0530

    Create file

---
+++ 49-GroupAnagrams/49-GroupAnagrams.cpp
@@ -0,0 +1,16 @@
+        for(string str:strs)
+        {
+            auto key = str;
+        }
+            sort(key.begin(),key.end());
+            mp[key].push_back(str);
+        while(iter!=mp.end()){
+        auto iter = mp.begin();
+            vec.push_back(iter->second);
+        }
+            iter++;
+        return vec;
+    }
+};
+        vector<vector<string>> vec;
+[
