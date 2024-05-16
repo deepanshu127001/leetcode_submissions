bc1f2890c0d33340d2533a320810ea06ae186f8f
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-17 02:28:32 +0530

    Create file

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,17 @@
+        vector<int> ans;
+        map<int,int> mp;
+        for(int val : nums){
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+class Solution {
+public:
+
+// };
+
+//     }
+//         return ans;
+            mp[val]++;
+        }
+        auto iter = mp.begin();
+        while(iter != mp.end()){
+            if(iter->second >= k) // Compare with k for top K frequent elements
+[
