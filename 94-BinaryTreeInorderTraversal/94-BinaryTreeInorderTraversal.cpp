9ea0fb17a4ca1a7b34d7d5adfc30fc53740ce549
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-05-29 22:29:08 +0530

    Create file

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,13 @@
+public:
+    vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (root != nullptr) {
+            inorderTraversal(root->left);
+            ans.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return ans;
+    }
+};
+
+[
