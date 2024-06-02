05e3ac243d7ad3790cc7c6dea76bbbddb34817c9
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-06-02 05:30:55 +0530

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.cpp
@@ -0,0 +1,20 @@
+            int queue_size = q.size();
+            if(queue_size==0)return ans;
+            while(queue_size > 0){
+                TreeNode *node = q.front();
+                q.pop();
+                nodes.push_back(node->val);
+                if(node->left)
+            vector<int> nodes;
+        while(1){
+            return ans;
+        if(root==nullptr)
+        q.push(root);
+        queue<TreeNode*> q;
+        vector<vector<int>> ans;
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            queue_size--;
+            }
+[
