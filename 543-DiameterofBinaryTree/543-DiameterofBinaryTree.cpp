fa756c74b5b3b543e911529cf599b5f595d05226
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2024-06-02 21:59:31 +0530

    Create file

---
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.cpp
@@ -0,0 +1,9 @@
+        ans = max(ans,1+lh+rh);
+        return max(lh+1,rh+1);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int data = height(root);
+        return ans-1;
+    }
+};
+[
