2dc59fa1e562968e92d2ad398a418f4a7b4cce0a
Deepanshu <96337435+deepanshu127001@users.noreply.github.com>
2025-03-03 02:10:22 +0530

    Create file

---
+++ 290-WordPattern/290-WordPattern.cpp
@@ -0,0 +1,24 @@
+    bool wordPattern(string pattern, string s) {
+        std::vector<std::string> words = tokenize(s);
+    }
+        while(iss >> token){
+            tokens.push_back(token);
+        }
+        return tokens;
+        std::unordered_map<std::string,char> strMap;
+        if(words.size()!=pattern.length())
+        return false;
+        for(auto index =0;index<words.size();index++){
+            if(strMap.find(word) == strMap.end()){
+            else if(strMap[word] == pattern[index])
+            std::string word = words[index];
+            continue;
+            else if(strMap[word] != pattern[index])
+            return false;
+        std::set<char> used;
+                strMap[word] = pattern[index];
+            }
+                used.insert(pattern[index]);
+                // if(used.find(pattern[index]!=used.end()))
+                return false;
+                if (used.find(pattern[index]) != used.end())
